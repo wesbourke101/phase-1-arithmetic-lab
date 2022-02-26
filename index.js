5d04cfd4ed6f3076be14ab9bb13decc3764c777e
Wes Bourke <wesbourkeavl@gmail.com>
2022-02-26 15:19:08 -0500

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
 
 // Write your code here
+let num1 = 2, num2 = 31;
+
+let multiply = num1 * num2;
+
+let random = Math.floor(Math.random() * 10) + 1;
+
+let num3 = 4, num4 = 5;
+let mod = num3 % num4;
+
+let max = Math.max(1,2,4,3,13,20,2,13,2,7,8,1)
